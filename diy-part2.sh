d1b68e617e1081f44e340f534e3a6f76a3b75726
ak3364 <102459435+ak3364@users.noreply.github.com>
2026-02-10 15:59:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,15 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+
+# 1. 修改主机名为 My-Router
+sed -i 's/hostname='OpenWrt'/hostname='USB-Server'/g' package/base-files/files/bin/config_generate
+
+# 2. 修改默认 SSID 为 My-WiFi
+sed -i 's/ssid=OpenWrt/ssid=USB-Server/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 3. 默认开启无线
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+ed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set wireless.default_radio${devidx}.key=12345678' package/kernel/mac80211/files/lib/wifi/mac80211.sh
